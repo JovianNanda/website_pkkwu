503ace48c4a99810eacf5a0faebfc92fa7f0b700
Jovian Nanda <joviansismawan13@gmail.com>
2023-12-07 15:20:29 +0800

    push update

--- assets/lenis.js
+++ assets/lenis.js
@@ -1,6 +1,18 @@
-import Lenis from '@studio-freight/lenis'
 
-const lenis = new Lenis()
+import Lenis from '/@studio-freight/lenis' 
+
+const lenis = new Lenis({
+  duration: 1.5, 
+  easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)), // https://easings.net
+  direction: 'vertical', 
+  smooth: true,
+  smoothTouch: false,
+  touchMultiplier: 1.5,
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+  lenis.start()
+})
 
 function raf(time) {
   lenis.raf(time)
@@ -8,3 +20,4 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+
